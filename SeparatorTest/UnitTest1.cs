3ec5baaed43880b65875e716af3582073d1b2dca
Paulo <pavel_syrovatsky@mail.ru>
2020-06-04 23:34:30 +0300

    End menu test

--- SeparatorTest/UnitTest1.cs
+++ SeparatorTest/UnitTest1.cs
@@ -50,9 +50,9 @@ namespace SeparatorTest
         {
             var consoleMok = new Mock<MyConsole>().As<IConsole>();
             consoleMok.SetupSequence(c => c.ReadLine())
-                .Returns("group")
-                .Returns("paul")
-                .Returns("1");
+                .Returns("group") // name of group
+                .Returns("paul") // name of first friend
+                .Returns("1");// find 1 group
             Input.myConsole = consoleMok.Object;
             Menu menu = new Menu();
             menu.CreateGroup();
@@ -71,12 +71,12 @@ namespace SeparatorTest
         {
             var consoleMok = new Mock<MyConsole>().As<IConsole>();
             consoleMok.SetupSequence(c => c.ReadLine())
-                .Returns("4")
-                .Returns("2")
-                .Returns("group")
-                .Returns("paul")
-                .Returns("7")
-                .Returns("5");
+                .Returns("4")// delete all group
+                .Returns("2") //try find some group, but create new group because groupd  not vreated yet
+                .Returns("group") // name of group
+                .Returns("paul")// name of friend
+                .Returns("7") // exit from main page
+                .Returns("5"); // exit
             Input.myConsole = consoleMok.Object;
             Menu menu = new Menu();
             menu.StartMenu();
@@ -88,6 +88,43 @@ namespace SeparatorTest
             }
             Group group2 = new Group("Group");
             Assert.AreEqual(group2.Name, group.Name);
+        }
+
+        [TestMethod]
+        public void TryInputErrorInt()
+        {
+            var consoleMok = new Mock<MyConsole>().As<IConsole>();
+            consoleMok.SetupSequence(c => c.ReadLine())
+                .Returns("7")//input outofarray
+                .Returns("5");//input correct number and exit
+            Input.myConsole = consoleMok.Object;
+            Menu menu = new Menu();
+            Assert.AreEqual(0, menu.StartMenu());
+        }
+
+        [TestMethod]
+        public void DeleteSelectedGroup()
+        {
+            var consoleMok = new Mock<MyConsole>().As<IConsole>();
+            consoleMok.SetupSequence(c => c.ReadLine())
+                .Returns("1") //try find some group, but create new group because groupd  not vreated yet
+                .Returns("groups") // name of group
+                .Returns("paul")// name of friend
+                .Returns("7") // exit from main page
+                .Returns("3")// delete all group
+                .Returns("1")// delete all group
+                .Returns("7") // exit from main pag
+                .Returns("5"); // exit
+            Input.myConsole = consoleMok.Object;
+            Menu menu = new Menu();
+            menu.StartMenu();
+            using (var db = new LiteDatabase(@"MyData.db"))
+            {
+                var col = db.GetCollection<Group>("group");
+                var group = col.FindOne(x => x.Name.Equals("Groups"));
+                Assert.AreEqual(null, group);
+
+            }
         }
 
 
